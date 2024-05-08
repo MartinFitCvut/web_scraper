51aba5c933e2129f72f76db13e26e3bbab8c8eaa
mt648sh <martin.thern@student.tuke.sk>
2024-05-08 16:06:29 +0200

    setup

--- setup.js
+++ setup.js
@@ -11,8 +11,20 @@ function runNpmInstall(directory) {
     });
 }
 
+function runSetupMongo() {
+    console.log('Spúšťam setupmongo.js...');
+    exec('cd backend && node ./src/setup/setupmongo.js', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Chyba pri spúšťaní setupmongo.js: ${error}`);
+            return;
+        }
+        console.log(`setupmongo.js: ${stdout}`);
+    });
+}
 // Spustí npm install v backend adresári
 runNpmInstall('./backend'); // Nahraďte '/cesta/k/backend' skutočnou cestou k backend adresáru
 
 // Spustí npm install v frontend/client_app adresári
 runNpmInstall('./frontend/client_app'); // Nahraďte '/cesta/k/frontend/client_app' skutočnou cestou k frontend/client_app adresáru
+
+runSetupMongo();
